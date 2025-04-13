865760f1e326c339fd9c287105bc0000c080ba4b
Yahiris05 <encarnayahiris@gmail.com>
2025-04-12 23:56:20 -0400

    Update Contacto.cs

--- Models/Contacto.cs
+++ Models/Contacto.cs
@@ -3,7 +3,7 @@
 public class Contacto
 {
     public int Id { get; set; }
-    public required string Nombre { get; set; }  // "required" es de .NET 7/8
-    public string? Telefono { get; set; }        // "?" indica que puede ser null
+    public required string Nombre { get; set; }  
+    public string? Telefono { get; set; }        
     public string? Email { get; set; }
 }
